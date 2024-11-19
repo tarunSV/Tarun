2f56d1d9eb2baeb91e426f33bb56035e3e0b9ee8
tarunSV <tarunsvasistha@gmail.com>
2024-11-19 19:06:03 +0530

    added

---
+++ ResultExample.java
@@ -0,0 +1,12 @@
+class ResultExample
+ {
+    public static void main(String[] arr) {
+        int number1 = 2;
+        int number2 = 4;
+        int result;
+
+	result = number1 + number2;
+
+        System.out.println("Result of 2 numbers " + result);
+    }
+}
